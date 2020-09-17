6cb3d6a1db82f81d3339315225c8cf0c1191d837
Ankit Agarwal <its_ankit1@yahoo.co.in>
2020-09-17 23:05:56 +0100

    adding capturing of ids from url

--- sites/boats.go
+++ sites/boats.go
@@ -1,11 +1,12 @@
 package sites
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
-	"strconv"
+	"regexp"
 	"strings"
 
 	"boatfuji.com/api"
@@ -14,6 +15,7 @@ import (
 var (
 	btBaseURL     = "https://www.boats.com/"
 	sourceBaseURL = "https://www.boattrader.com"
+	btURLPattern  = regexp.MustCompile(`^https://www\.boats\.com/boat/([0-9]*)`)
 	btBaseDir     = "/Users/ankitagarwal/ankit_code/boat-golang/harvest/www.boats.com/"
 )
 
@@ -30,47 +32,42 @@ type Boats struct {
 // Harvest gets data from the site
 func (site *Boats) Harvest(url string) error {
 	assetsdir := []string{"boat-sales", "boats"}
-	assets := []string{"boats"}
 	for _, assetdir := range assetsdir {
 		os.MkdirAll(btBaseDir+assetdir, 0755)
 	}
 	switch url {
-	case "":
-		for _, asset := range assets {
-			files, err := ioutil.ReadDir(btBaseDir + asset)
-			if err != nil {
-				return err
-			}
-			oldPercent := ""
-			for fileIndex, file := range files {
-				newPercent := strconv.Itoa(100 * fileIndex / len(files))
-				if newPercent != oldPercent {
-					log.Println(asset + " " + newPercent + "%")
-				}
-				oldPercent = newPercent
-				var fileName = file.Name()
-				if strings.HasSuffix(fileName, ".htm") {
-					// id := strings.Split(filepath.Base(fileName), ".")[0]
-					// if _, err = site.harvestX(asset, id); err != nil {
-					// 	return err
-					// }
-				}
-			}
-		}
 	case btBaseURL:
+		boatsByFilters = map[string][]string{}
 		for _, filter := range []string{"type-power", "type-sail", "type-small", "type-pwc"} {
 			for page := 1; page < 99999; page++ {
-				log.Println(fmt.Sprintf("%s/boat-sales/%d&%s.html", btBaseDir, page, filter))
 				boatsPage, err := getPage(fmt.Sprintf("%s/boat-sales/%d&%s.html", btBaseDir, page, filter), fmt.Sprintf("%s/boats/%s/page-%d/", sourceBaseURL, filter, page))
 				if err != nil {
 					return err
 				}
-				bsBoatIDs := boatsPage.FindN(nil, "//li/@data-listing-id", 0, 99999999, "", "")
-				log.Println(bsBoatIDs)
+				bsBoatIDs := boatsPage.FindN(nil, "//li/@data-listing-id", 0, 9999999, "", "")
+				if len(bsBoatIDs) != 0 {
+					filtersKey := strings.Split(filter, "-")[1]
+					boatsByFilters[filtersKey] = append(bsBoatIDs, boatsByFilters[filter]...)
+				} else {
+					break
+				}
 			}
 		}
-	default:
+		filtersJSON, _ := json.Marshal(boatsByFilters)
+		ioutil.WriteFile(btBaseDir+"filters.json", filtersJSON, 0644)
 		return nil
+	}
+	match := btURLPattern.FindStringSubmatch(url)
+	if match == nil {
+		return errors.New("BadURL")
 	}
 	return nil
 }
+
+// func (site *Boats) harvestX(x, id string) (int64, error) {
+// 	return site.harvestBoat(id, 0, nil)
+// }
+
+// func (site *Boats) harvestBoat(x, id string) (int64, error) {
+// 	return site.harvestBoat(id, 0, nil)
+// }
